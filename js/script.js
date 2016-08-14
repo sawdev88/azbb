b3b26f0ecb13d906869bf277fa0df34f8a8f2fef
Sean Wines <seanwinesdev@gmail.com>
2016-08-14 12:10:17 -0700

    Added about me and contact section. Added click listener to body to close side menu

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
-// Show nav when hamburger icon is clicked
-$('.show-nav').on('click', function () {
-  $('.side-nav').animate({ right: 0 });
-});
 
-// hide nav when remove icon is clicked
-$('.hide-nav').on('click', function () {
-  $('.side-nav').animate({ right: -220 });
+// Show nav when hamburger icon is clicked
+$('.show-nav').on('click', function (e) {
+  e.stopPropagation();
+  $('.side-nav').animate({ right: 0 });
 })
+
+var hideNav = function () {
+  // hide nav when remove icon is clicked
+  $('.side-nav').animate({ right: -220 });
+};
+
+// Hide nav when hide-nav icon is clicked
+$('.hide-nav').on('click', hideNav );
+
+// Hide nav when body is clicked
+$('body').on('click', hideNav );
 
 // Flash down arrow
 $('.down-arrow').delay(1000).fadeIn(1000).delay(500).fadeOut('slow');
