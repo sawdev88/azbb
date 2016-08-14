8b9a98d47a37cd6db696e797c8e671603022f206
Sean Wines <seanwinesdev@gmail.com>
2016-08-13 20:33:35 -0700

    Added flash down arrow

--- js/script.js
+++ js/script.js
@@ -8,3 +8,6 @@ $('.show-nav').on('click', function () {
 $('.hide-nav').on('click', function () {
   $('.side-nav').animate({ right: -220 });
 })
+
+// Flash down arrow
+$('.down-arrow').delay(1000).fadeIn(1000).delay(500).fadeOut('slow');
